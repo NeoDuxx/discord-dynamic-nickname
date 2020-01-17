e00e21eb4697553e7b2f1dd2169ba8bee4e751bb
woah1337 <40292926+woah1337@users.noreply.github.com>
2020-01-17 21:05:43 +0530

    asjda=sifaguwwaigaigt

---
+++ index.js
@@ -0,0 +1,71 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+//Your token
+var token = 'hhtwqhthhtwt.S-quiwtuwtu-qrqjrqr';
+//Your server id (right click server and copy id)
+var guildId = '157175177257275';
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  setInterval(function(){setNick()}, 500);
+});
+
+
+client.login(token);
+
+  var counter = 1;
+  function setNick(){
+    var server = client.guilds.get(guildId);
+    var member = server.members.get(client.user.id);
+       switch(counter)
+       {
+           case 1:
+               member.setNickname('D');
+               break;
+               case 2:
+                   member.setNickname('Do');
+                   break;
+                   case 3:
+                       member.setNickname('Dok');
+                       break;
+                       case 4:
+                           member.setNickname('Doki')
+                           break;
+                           case 5:
+                               member.setNickname('DokiD');
+                               break;
+                               case 6:
+                                   member.setNickname('DokiDo');
+                                   break;
+                                   case 7:
+                                       member.setNickname('DokiDok');
+                                       break;
+                                       case 8:
+                                           member.setNickname('DokiDoki')
+                                           break;
+                                           case 9:
+                                               member.setNickname('DokiDok');
+                                               break;
+                                               case 10:
+                                                   member.setNickname('DokiDo');
+                                                   break;
+                                                   case 11:
+                                                       member.setNickname('DokiD');
+                                                       break;
+                                                       case 12:
+                                                           member.setNickname('Doki');
+                                                           break;
+                                                           case 13:
+                                                               member.setNickname('Dok');
+                                                               break;
+                                                               case 14:
+                                                                   member.setNickname('Do');
+                                                                   break;
+                                                                   case 15:
+                                                                       counter = 0;
+                                                                       break;
+       }
+       counter++;
+  }
